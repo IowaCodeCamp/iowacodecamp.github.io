fecb4d52ae755b307207b2788bbc0e2588a1fcd4
ICC CI <support@iowacodecamp.com>
2026-03-14 01:34:24 +0000

    Deployment from Website repo: commit

--- common.js
+++ common.js
@@ -7,7 +7,7 @@
   \*****************************************/
 /***/ ((module) => {
 
-module.exports = JSON.parse('[{"title":"President, Finances, Logistics, Volunteers","firstName":"Greg","lastName":"Sohl","profilePicture":"/assets/images/greg-sohl.jpg"},{"title":"Sponsorship, Speakers, Emails & Communication, Networking Events","firstName":"Nick","lastName":"Parker","profilePicture":"/assets/images/nick-parker.jpg"},{"title":"Sponsorship, Speakers, Event Planning","firstName":"Juan","lastName":"Moya","profilePicture":"/assets/images/juan-moya.jpg"},{"title":"Sponsorship, Speakers","firstName":"Mike","lastName":"Cole","profilePicture":"/assets/images/mike-cole.jpg"},{"title":"Sponsorship, Speakers","firstName":"Levi","lastName":"Rosol","profilePicture":"/assets/images/levi-rosol.jpg"}]');
+module.exports = JSON.parse('[{"title":"President, Finances, Logistics, Volunteers","firstName":"Greg","lastName":"Sohl","profilePicture":"/assets/images/greg-sohl.jpg"},{"title":"Sponsorship, Speakers, Emails & Communication, Networking Events","firstName":"Nick","lastName":"Parker","profilePicture":"/assets/images/nick-parker.jpg"},{"title":"Sponsorship, Speakers","firstName":"Mike","lastName":"Cole","profilePicture":"/assets/images/mike-cole.jpg"},{"title":"Sponsorship, Speakers","firstName":"Levi","lastName":"Rosol","profilePicture":"/assets/images/levi-rosol.jpg"}]');
 
 /***/ })
 
